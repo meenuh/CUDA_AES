da2cd66bc695dbe9d4bf62e65225b429bba53c1f
mina yi <dearmeenuh@gmail.com>
2017-05-01 22:36:14 -0700

    adding AES init function

--- aes.cu
+++ aes.cu
@@ -8,15 +8,14 @@
 
 #define ENCRYPT 0
 #define DECRYPT 1
+#define KEYSIZE 16
 
 typedef struct {
 	int mode;
-	char *key;
+	char key[KEYSIZE];
 	char *output_filename;
 	FILE *fin;
 }AES_INFO;
-
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
 /**
  Returns pointer to struct containing information from command line -- 4 arguments expected
@@ -35,114 +34,12 @@ __global__ void addKernel(int *c, const int *a, const int *b)
     c[i] = a[i] + b[i];
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
-
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
-
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
-
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
+	AES_INFO *data = get_args(argv, argc);
 
     return 0;
 }
-
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
-
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
-}
-
 
 //arg[0] - program name - not used
 //arg[1] - Encrypt or decrypt(e for encrypt, d for decrypt)
@@ -157,7 +54,7 @@ AES_INFO *get_args(char **argv, int argc) {
 	}
 
 	if (argc != 5) {
-		printf("Incorrect number of arguments\n");
+		printf("Incorrect number of arguments. Found %d\n", argc);
 		exit(EXIT_FAILURE);
 	}
 
@@ -165,7 +62,7 @@ AES_INFO *get_args(char **argv, int argc) {
 	case 'e':
 		info->mode = ENCRYPT;
 		break;
-	case 'd': 
+	case 'd':
 		info->mode = DECRYPT;
 		break;
 	default:
@@ -173,5 +70,35 @@ AES_INFO *get_args(char **argv, int argc) {
 		exit(EXIT_FAILURE);
 	}
 
-	int len = strle
+	//get the key
+	char buffer[KEYSIZE];
+
+	FILE *key_fp = fopen(argv[2], "rb");
+	if (NULL == key_fp) {
+		printf("Error opening keyfile: %s\n", argv[2]);
+		exit(EXIT_FAILURE);
+	}
+
+	fread(buffer, 1, KEYSIZE, key_fp);
+	strncpy(info->key, buffer, KEYSIZE);
+	
+	printf("Using key: ");
+	for (int i = 0; i < KEYSIZE; i++) {
+		printf("%02x ", info->key[i]);
+	}
+
+	//get FILE * to file to encrypt/decrypt
+	info->fin = fopen(argv[3], "rb");
+	if (NULL == info->fin) {
+		printf("Error opening input file %s\n", argv[3]);
+		exit(EXIT_FAILURE);
+	}
+	printf("\nUsing input file %s\n", argv[3]);
+
+	//get output file name to be used later
+	info->output_filename = (char *)malloc(strlen(argv[4]) + 1);
+	strcpy(info->output_filename, argv[4]);
+	printf("Output file is %s\n", argv[4]);
+
+	return info;
 }
