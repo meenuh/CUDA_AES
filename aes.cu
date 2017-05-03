f7e225945a34333bc81521351a717d63d540b37e
saar4ever <saarvx@gmail.com>
2017-05-03 01:57:16 -0700

    Key Expansion + other changes

--- aes.cu
+++ aes.cu
@@ -8,11 +8,58 @@
 
 #include "aes.h"
 
-__constant__ unsigned char D_SBOX[16][16];
-__constant__ unsigned char D_INV_SBOX[16][16];
+__constant__ byte D_SBOX[16][16];
+__constant__ byte D_INV_SBOX[16][16];
 
-__device__ void keyExpansion() {
+//Global expanded key array - 44 words (4 words per round * 11 round keys needed)
+byte ExpandKey[44][4];
 
+//Global state matrix which will store the data on each AES round
+byte State[4][4];
+
+__host__ void keyExpansion(byte * Key) {
+	//Temp word to store the previous word and to rotate word
+	byte temp[4], rtemp[4];
+	
+	//Word iteration (0-3). 4 words is 1 round key.
+    int word_it;
+	
+	//Copies the first 8 words. First 4 is first round key. Second 4 used to generate next round key
+    memcpy(ExpandKey, Key, 32);
+
+	//Calculate the rest of the words for the expanded key
+	//44 rounds to generate 11 round keys, first already created
+	for (int Round_it = 4; Round_it < 44; Round_it++) { 
+        
+		//Get the last created word
+        memcpy(temp, ExpandKey[Round_it - 1], 4);
+		
+		//This only occurs in every 4 rounds iterations (last word in a round)
+        if ((Round_it % 4) == 0) { 
+			
+			//Every 4th word, rotate by 1 byte, apply SBOX, and XOR RCON
+			rtemp[4] = {temp[0], temp[1], temp[2], temp[3]};
+			for (word_it = 0; word_it < 4; word_it++) {
+				temp[word_it] = rtemp[(word_it + 1) % 4];
+			}
+			
+			//Apply SBOX (using host const SBOX, not D_SBOX)
+            for (word_it = 0; word_it < 4; ++word_it){
+				temp[word_it] = SBOX[(temp[word_it]&0xf0) >> 4][temp[word_it]&0x0f];
+			}
+        
+			//XOR of the first element with the corresponding RCON value (elements 2-4 are always 0 in RCON)
+            temp[0] ^= RCon[Round_it / 4];
+        }
+        
+		//For all the words, apply an XOR with the block 4 bytes before the current one
+        for (word_it = 0; word_it < 4; word_it++) {
+			temp[word_it] ^= ExpandKey[Round_it - 4][word_it];
+        }
+		
+		// Store the new word on th expanded key
+        memcpy(ExpandKey[Round_it], temp, 4);
+	}
 }
 
 __global__ void AESKernel(char *x)
@@ -23,36 +70,42 @@ __global__ void AESKernel(char *x)
 
 int main(int argc, char **argv)
 {
+	//CUDA error variable
+    cudaError_t cuda_error;
+	
 	AES_INFO *data = get_args(argv, argc);
 	char *h = (char *)malloc(256);
 	for (int i = 0; i < 256; i++)
 		h[i] = i;
 
-	char *arr;
-	cudaMalloc((void**)&arr, sizeof(char) * 256);
+	byte *arr;
+	cudaMalloc((void**)&arr, sizeof(byte) * 256);
 	cudaMemset(arr, 0, sizeof(char)*256);
 
-	if (cudaMemcpyToSymbol(D_SBOX, SBOX, sizeof(char) * 256) != cudaSuccess) {
-		printf("error on copy");
+	cuda_error = cudaMemcpyToSymbol(D_SBOX, SBOX, sizeof(byte) * 256);
+	if (cuda_error != cudaSuccess) {
+		printf("error on copy SBOX");
 	}
-	if (cudaMemcpyToSymbol(D_INV_SBOX, INVERSE_SBOX, sizeof(char) * 256) != cudaSuccess) {
-		printf("error on copy");
+	cuda_error = cudaMemcpyToSymbol(D_INV_SBOX, INVERSE_SBOX, sizeof(byte) * 256)
+	if (cuda_error != cudaSuccess) {
+		printf("error on copy INV_SBOX");
 	}
 
 	cudaMemcpy(arr, h, 256, cudaMemcpyHostToDevice);
 
 	dim3 block(16, 16);
-
-	AESKernel << <1, block>> > (arr);
-	cudaError_t err = cudaGetLastError();
-	if(err != cudaSuccess)
-		fprintf(stderr, "err %s", cudaGetLastError());
-
+	
+	keyExpansion(data->key);
+	AESKernel <<<1, block>>> (arr);
+	cuda_error = cudaGetLastError();
+	if(cuda_error != cudaSuccess){
+		fprintf(stderr, "err %s", cuda_error);
+	}
 	cudaMemcpy(h, arr, 256, cudaMemcpyDeviceToHost);
     return 0;
 }
 
-//arg[0] - program name - not used
+//arg[0] - program name - not used - What is this for? Remove it and shift down the others?
 //arg[1] - Encrypt or decrypt(e for encrypt, d for decrypt)
 //arg[2] - Filename containing our key
 //arg[3] - Input file to encrypt / decrypt
@@ -81,8 +134,8 @@ AES_INFO *get_args(char **argv, int argc) {
 		exit(EXIT_FAILURE);
 	}
 
-	//get the key
-	char buffer[KEYSIZE];
+	//get the key file
+	//char buffer[KEYSIZE]; //Why do you need a buffer for key file read?
 
 	FILE *key_fp = fopen(argv[2], "rb");
 	if (NULL == key_fp) {
@@ -90,15 +143,19 @@ AES_INFO *get_args(char **argv, int argc) {
 		exit(EXIT_FAILURE);
 	}
 
-	fread(buffer, 1, KEYSIZE, key_fp);
-	strncpy(info->key, buffer, KEYSIZE);
+	fread(info->key, 1, KEYSIZE, key_fp);
+	//strncpy(info->key, buffer, KEYSIZE);
 	
 	printf("Using key: ");
 	for (int i = 0; i < KEYSIZE; i++) {
+		if(info->key[1] == NULL){
+			printf("Error: Invalid key file, missing value at %d\n", i);
+			exit(EXIT_FAILURE);
+		}
 		printf("%02x ", info->key[i]);
 	}
 
-	//get FILE * to file to encrypt/decrypt
+	//get the input FILE * to encrypt/decrypt
 	info->fin = fopen(argv[3], "rb");
 	if (NULL == info->fin) {
 		printf("Error opening input file %s\n", argv[3]);
@@ -106,7 +163,7 @@ AES_INFO *get_args(char **argv, int argc) {
 	}
 	printf("\nUsing input file %s\n", argv[3]);
 
-	//get output file name to be used later
+	//get the output file name to be used later
 	info->output_filename = (char *)malloc(strlen(argv[4]) + 1);
 	strcpy(info->output_filename, argv[4]);
 	printf("Output file is %s\n", argv[4]);
